89e17e5b64fdec31938517bf51eb0873e7d8f0fb
harishn-kireetisoft <73819723+harishn-kireetisoft@users.noreply.github.com>
2020-12-23 18:42:09 +0530

    Delete Get operationcompleted

--- ANGULARJS/SCRIPTS/Angularcode.js
+++ ANGULARJS/SCRIPTS/Angularcode.js
@@ -2,109 +2,156 @@
 // For Checkbox
 
 var app = angular.module('myApp', [])
-app.controller('MyController', function ($scope, $window) {
-    $scope.Hobbies = [{
-        hobbyid: 1,
-        Name: 'Cricket',
-        Selected: false
-    }, {
-        hobbyid: 2,
-        Name: 'Songs',
-        Selected: false
-    }, {
-        hobbyid: 3,
-        Name: 'Chess',
-        Selected: false
-    }];
+app.controller('MyController', function ($scope, $http) {
+    //$scope.Hobbies = [{
+    //    HobbieId: '1',
+    //    HobbieName: 'Cricket',
+    //    checked: false
+    //}, {
+    //    HobbieId: '2',
+    //    HobbieName: 'Songs',
+    //    checked: false
+    //}, {
+    //    HobbieId: '3',
+    //    HobbieName: 'Chess',
+    //    checked: false
+    //}];
+    $scope.InsertData = function () {
+        
+        var Action = document.getElementById("btnInsert").getAttribute("value");
+        if (Action == "insert") {
+            debugger;
+            alert("hello");
+            $scope.emp = {};
+            $scope.emp.Ename =   $scope.EmpName;
+            $scope.emp.Salary =  $scope.EmpSalary;
+            $scope.emp.Gender =  $scope.selectedvalue;
+            $scope.emp.Hobbies = $scope.selectedvalue0;
+            $scope.emp.Hobbies1 = $scope.selectedvalue1;
+            $scope.emp.Hobbies2 = $scope.selectedvalue2;
+            $scope.emp.City =    $scope.Selectlist;
 
-    $scope.GetValue = function () {
-        var message = "";
-        for (var i = 0; i < $scope.Hobbies.length; i++) {
-            if ($scope.Hobbies[i].Selected) {
-                var fruitId = $scope.Hobbies[i].hobbyid;
-                var fruitName = $scope.Hobbies[i].Name;
-                message += "Value: " + fruitId + " Text: " + fruitName + "\n";
-                $window.alert(message);
-            }
+            $http({
+                method: "post",
+                url: 'Employee/AddEmployee',
+                datatype: "json",
+                data: JSON.stringify($scope.emp)
+            }).then(function (response) {
+                alert(response.data);
+                $scope.EmpName;
+                $scope.EmpSalary;
+                $scope.selectedvalue;
+                $scope.selectedvalue0;
+                $scope.selectedvalue1;
+                $scope.selectedvalue2;
+                $scope.Selectlist;
+                GetAllData
+            })
         }
-    }
+      
+    };
+    $scope.GetAllData = function () {
+
+        $http({
+            method: "get",
+            url: 'Employee/List',
+        }).then(function (response) {
+            $scope.employees = response.data;
+        }, function () {
+            alert("Error Occur");
+        })
+    };
+    $scope.DeleteEmp = function (Emp) {
+        
+        $http({
+            method: "post",
+            url: 'Employee/Delete',
+            datatype: "json",
+            data: JSON.stringify(Emp)
+        }).then(function (response) {
+            $scope.GetAllData();
+        });
+    };
+  
 });
-// For insert get update delete
-var app = angular.module("Crud", []);  
-app.controller("CRUDCONTROL", function($scope, $http) {  
-    debugger;  
-    $scope.InsertData = function() {  
-        var Action = document.getElementById("btnSave").getAttribute("value");  
-        if (Action == "Submit") {  
-            $scope.Employe = {};  
-            $scope.Employe.Emp_Name = $scope.EmpName;  
-            $scope.Employe.Emp_City = $scope.EmpCity;  
-            $scope.Employe.Emp_Age = $scope.EmpAge;  
-            $http({  
-                method: "post",  
-                url: "http://localhost:39209/Employee/Insert_Employee",  
-                datatype: "json",  
-                data: JSON.stringify($scope.Employe)  
-            }).then(function(response) {  
-                alert(response.data);  
-                $scope.GetAllData();  
-                $scope.EmpName = "";  
-                $scope.EmpCity = "";  
-                $scope.EmpAge = "";  
-            })  
-        } else {  
-            $scope.Employe = {};  
-            $scope.Employe.Emp_Name = $scope.EmpName;  
-            $scope.Employe.Emp_City = $scope.EmpCity;  
-            $scope.Employe.Emp_Age = $scope.EmpAge;  
-            $scope.Employe.Emp_Id = document.getElementById("EmpID_").value;  
-            $http({  
-                method: "post",  
-                url: "http://localhost:39209/Employee/Update_Employee",  
-                datatype: "json",  
-                data: JSON.stringify($scope.Employe)  
-            }).then(function(response) {  
-                alert(response.data);  
-                $scope.GetAllData();  
-                $scope.EmpName = "";  
-                $scope.EmpCity = "";  
-                $scope.EmpAge = "";  
-                document.getElementById("btnSave").setAttribute("value", "Submit");  
-                document.getElementById("btnSave").style.backgroundColor = "cornflowerblue";  
-                document.getElementById("spn").innerHTML = "Add New Employee";  
-            })  
-        }  
-    }  
-    $scope.GetAllData = function() {  
-        $http({  
-            method: "get",  
-            url: "http://localhost:39209/Employee/Get_AllEmployee"  
-        }).then(function(response) {  
-            $scope.employees = response.data;  
-        }, function() {  
-            alert("Error Occur");  
-        })  
-    };  
-    $scope.DeleteEmp = function(Emp) {  
-        $http({  
-            method: "post",  
-            url: "http://localhost:39209/Employee/Delete_Employee",  
-            datatype: "json",  
-            data: JSON.stringify(Emp)  
-        }).then(function(response) {  
-            alert(response.data);  
-            $scope.GetAllData();  
-        })  
-    };  
-    $scope.UpdateEmp = function(Emp) {  
-        document.getElementById("EmpID_").value = Emp.Emp_Id;  
-        $scope.EmpName = Emp.Emp_Name;  
-        $scope.EmpCity = Emp.Emp_City;  
-        $scope.EmpAge = Emp.Emp_Age;  
-        document.getElementById("btnSave").setAttribute("value", "Update");  
-        document.getElementById("btnSave").style.backgroundColor = "Yellow";  
-        document.getElementById("spn").innerHTML = "Update Employee Information";  
-    }  
-})  
-Now, provide the references of AngularJS and AngularCode file that we created into Index View.
+
+
+//// For insert get update delete
+//var app = angular.module("Crud", []);  
+//app.controller("CRUDCONTROL", function($scope, $http) {  
+//    debugger;  
+//    $scope.InsertData = function() {  
+//        var Action = document.getElementById("btnSave").getAttribute("value");  
+//        if (Action == "Submit") {  
+//            $scope.Employe = {};  
+//            $scope.Employe.Emp_Name = $scope.EmpName;  
+//            $scope.Employe.Emp_City = $scope.EmpCity;  
+//            $scope.Employe.Emp_Age = $scope.EmpAge;  
+//            $http({  
+//                method: "post",  
+//                url: "http://localhost:39209/Employee/Insert_Employee",  
+//                datatype: "json",  
+//                data: JSON.stringify($scope.Employe)  
+//            }).then(function(response) {  
+//                alert(response.data);  
+//                $scope.GetAllData();  
+//                $scope.EmpName = "";  
+//                $scope.EmpCity = "";  
+//                $scope.EmpAge = "";  
+//            })  
+//        } else {  
+//            $scope.Employe = {};  
+//            $scope.Employe.Emp_Name = $scope.EmpName;  
+//            $scope.Employe.Emp_City = $scope.EmpCity;  
+//            $scope.Employe.Emp_Age = $scope.EmpAge;  
+//            $scope.Employe.Emp_Id = document.getElementById("EmpID_").value;  
+//            $http({  
+//                method: "post",  
+//                url: "http://localhost:39209/Employee/Update_Employee",  
+//                datatype: "json",  
+//                data: JSON.stringify($scope.Employe)  
+//            }).then(function(response) {  
+//                alert(response.data);  
+//                $scope.GetAllData();  
+//                $scope.EmpName = "";  
+//                $scope.EmpCity = "";  
+//                $scope.EmpAge = "";  
+//                document.getElementById("btnSave").setAttribute("value", "Submit");  
+//                document.getElementById("btnSave").style.backgroundColor = "cornflowerblue";  
+//                document.getElementById("spn").innerHTML = "Add New Employee";  
+//            })  
+//        }  
+//    }  
+//    $scope.GetAllData = function() {  
+//        $http({  
+//            method: "get",  
+//            url: "http://localhost:39209/Employee/Get_AllEmployee"  
+//        }).then(function(response) {  
+//            $scope.employees = response.data;  
+//        }, function() {  
+//            alert("Error Occur");  
+//        })  
+//    };  
+//    $scope.DeleteEmp = function(Emp) {  
+//        $http({  
+//            method: "post",  
+//            url: "http://localhost:39209/Employee/Delete_Employee",  
+//            datatype: "json",  
+//            data: JSON.stringify(Emp)  
+//        }).then(function(response) {  
+//            alert(response.data);  
+//            $scope.GetAllData();  
+//        })  
+//    };  
+//    $scope.UpdateEmp = function(Emp) {  
+//        document.getElementById("EmpID_").value = Emp.Emp_Id;  
+//        $scope.EmpName = Emp.Emp_Name;  
+//        $scope.EmpCity = Emp.Emp_City;  
+//        $scope.EmpAge = Emp.Emp_Age;  
+//        document.getElementById("btnSave").setAttribute("value", "Update");  
+//        document.getElementById("btnSave").style.backgroundColor = "Yellow";  
+//        document.getElementById("spn").innerHTML = "Update Employee Information";  
+//    }  
+//})  
+//Now, provide the references of AngularJS and AngularCode file that we created into Index View.
 
--- DAL/EmployeeRegistration.cs
+++ DAL/EmployeeRegistration.cs
@@ -9,49 +9,151 @@ using System.Data.SqlClient;
 using DAL.DALPROPERTIES;
 namespace DAL
 {
-   public class EmployeeRegistration
+    public class EmployeeRegistration
     {
-       DAL.DALPROPERTIES.Employeereg prop=new DALPROPERTIES.Employeereg();
-       string connection = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
-       public int Add(Employeereg emp)
-       {
-           int i;
-           using (SqlConnection con = new SqlConnection(connection))
-           {
-               con.Open();
-               SqlCommand com = new SqlCommand("SP_InsertEmp", con);
-               com.CommandType = CommandType.StoredProcedure;
-               com.Parameters.AddWithValue("@Ename", emp.EmpName);
-               com.Parameters.AddWithValue("@Salary", emp.salary);
-               com.Parameters.AddWithValue("@Gender", emp.Gender);
-               com.Parameters.AddWithValue("@Hobbies", emp.hobbies);
-               com.Parameters.AddWithValue("@City", emp.city);
-               i = com.ExecuteNonQuery();
-           }
-           return i;
-       }
-       public List<Employeereg> ListAll()
-       {
-           List<Employeereg> lst = new List<Employeereg>();
-           using (SqlConnection con = new SqlConnection(connection))
-           {
-               con.Open();
-               SqlCommand com = new SqlCommand("SP_Getdetails", con);
-               com.CommandType = CommandType.StoredProcedure;
-               SqlDataReader rdr = com.ExecuteReader();
-               while (rdr.Read())
-               {
-                   lst.Add(new Employeereg
-                   {
-                       Eno = Convert.ToInt32(rdr["Eno"]),
-                       EmpName=rdr["Ename"].ToString(),
-                       salary=Convert.ToDouble(rdr["Salary"]),
-                        Gender= Convert.ToBoolean(rdr["Gender"].ToString()),
-                       hobbies= rdr["Hobbies"].ToString(),
-                       city=rdr["City"].ToString(),
-                   });               }
-               return lst;
-           }
-       }
+        DAL.DALPROPERTIES.Employeereg prop = new DALPROPERTIES.Employeereg();
+        string connection = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+        public int Add(Employeereg emp)
+        {
+            int i;
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                con.Open();
+                SqlCommand com = new SqlCommand("SP_InsertEmp", con);
+                com.CommandType = CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("@Ename", emp.Ename);
+                com.Parameters.AddWithValue("@Salary", emp.Salary);
+                com.Parameters.AddWithValue("@Gender", emp.Gender);
+
+                if (emp.Hobbies == "True")
+                {
+                    //var a = "Cricket";
+                    emp.Hobbies = "Cricket";
+                    com.Parameters.AddWithValue("@Hobbies", Convert.ToString(emp.Hobbies));
+                }
+                else
+                {
+                    var b = "Not selecteed";
+
+                    emp.Hobbies = "Not selecteed";
+                    com.Parameters.AddWithValue("@Hobbies", Convert.ToString(emp.Hobbies));
+                }
+                if (emp.Hobbies1 == "True")
+                {
+
+                    var c = "Songs";
+                    emp.Hobbies1 = "Songs";
+                    com.Parameters.AddWithValue("@Hobbies1", Convert.ToString(emp.Hobbies1));
+                }
+                else
+                {
+                    var d = "Not selecteed";
+                    emp.Hobbies1 = "Not selecteed";
+                    com.Parameters.AddWithValue("@Hobbies1", Convert.ToString(emp.Hobbies1));
+                }
+                if (emp.Hobbies2 == "True")
+                {
+                    var e = "Chess";
+                   emp.Hobbies2 = "Chess";
+                    com.Parameters.AddWithValue("@Hobbies2", Convert.ToString(emp.Hobbies2));
+                }
+                else
+                {
+                    var f = "Not selecteed";
+                     emp.Hobbies2 = "Not selecteed";
+
+                    com.Parameters.AddWithValue("@Hobbies2", Convert.ToString(emp.Hobbies2));
+                }
+                com.Parameters.AddWithValue("@City", emp.City);
+                i = com.ExecuteNonQuery();
+            }
+            return i;
+        }
+        public List<Employeereg> ListAll()
+        {
+            List<Employeereg> lst = new List<Employeereg>();
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                con.Open();
+                SqlCommand com = new SqlCommand("SP_Getdetails", con);
+                com.CommandType = CommandType.StoredProcedure;
+                SqlDataReader rdr = com.ExecuteReader();
+                while (rdr.Read())
+                {
+                    lst.Add(new Employeereg
+                    {
+                        Eno = Convert.ToInt32(rdr["Eno"]),
+                        Ename = rdr["Ename"].ToString(),
+                        Salary = Convert.ToDouble(rdr["Salary"]),
+                        Gender = rdr["Gender"].ToString(),
+                        Hobbies = rdr["Hobbies"].ToString(),
+                        Hobbies1 = rdr["Hobbies1"].ToString(),
+                        Hobbies2 = rdr["Hobbies2"].ToString(),
+                        City = rdr["City"].ToString(),
+                    });
+                }
+                return lst;
+            }
+        }
+        public int DeleteEmployee(Employeereg Emp)
+        {
+            int i;
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                con.Open();
+                SqlCommand com = new SqlCommand("SP_DeleteEmployee", con);
+                com.CommandType = CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("@Id", Emp.Eno);
+                i = com.ExecuteNonQuery();
+            }
+            return i;
+        }
+        //public int Update(Registration emp)
+        //{
+        //    int i;
+        //    using (SqlConnection con = new SqlConnection(cs))
+        //    {
+        //        con.Open();
+        //        SqlCommand com = new SqlCommand("UpdateEmployee", con);
+        //        com.CommandType = CommandType.StoredProcedure;
+        //        com.Parameters.AddWithValue("@Id", emp.Id);
+        //        com.Parameters.AddWithValue("@Name", emp.Name);
+        //        com.Parameters.AddWithValue("@Password", emp.Password);
+        //        com.Parameters.AddWithValue("@DOB", emp.DOB);
+        //        com.Parameters.AddWithValue("@Gender", emp.Gender);
+        //        com.Parameters.AddWithValue("@Address", emp.Address);
+        //        com.Parameters.AddWithValue("@City", emp.City);
+        //        if (emp.Hobbies1 != "Cricket")
+        //        {
+        //            var b = "Not Selected";
+        //            com.Parameters.AddWithValue("@Hobbies1", b);
+        //        }
+        //        else
+        //        {
+        //            com.Parameters.AddWithValue("@Hobbies1", emp.Hobbies1);
+        //        }
+        //        if (emp.Hobbies2 != "Songs")
+        //        {
+        //            var b = "Not Selected";
+        //            com.Parameters.AddWithValue("@Hobbies2", b);
+        //        }
+        //        else
+        //        {
+        //            com.Parameters.AddWithValue("@Hobbies2", emp.Hobbies2);
+        //        }
+        //        if (emp.hobbies3 != "Chess")
+        //        {
+        //            var b = "Not Selected";
+        //            com.Parameters.AddWithValue("@hobbies3", b);
+        //        }
+        //        else
+        //        {
+        //            com.Parameters.AddWithValue("@hobbies3", emp.hobbies3);
+        //        }
+        //        i = com.ExecuteNonQuery();
+        //    }
+        //    return i;
+        //}
     }
+
 }
